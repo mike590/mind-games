da466aeb27dab0592559c17d7f3ca510b5924a97
mike590 <mike.gherard@gmail.com>
2015-01-28 21:12:35 -0500

    Added navbar with link to Stacks game and style changes on hover. Added stacks-board, which is just a ceneted square div with a black border. Mapped the path '/stacks' to erb stacks/game.erb.

---
+++ controllers/application_controller.rb
@@ -0,0 +1,15 @@
+class ApplicationController < Sinatra::Base
+
+  set :views, File.expand_path("../../public/views", __FILE__)
+  set :public_folder, File.expand_path("../../public", __FILE__)
+
+  get '/' do
+    erb :index
+  end
+
+  get '/stacks' do
+    erb :"stacks/game"
+  end
+
+
+end

56b927414ed7f45b6b37c82b91762ad48d729f01
mike590 <mike.gherard@gmail.com>
2015-01-30 17:23:51 -0500

    Created two viewports and two box and two cameras to view the boxes form different angles.

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,7 +1,7 @@
 console.log("Loaded!")
 
-var scene, camera, renderer;
-var geometry, material, mesh;
+var scene, camera1, camera2, renderer, container, containerWidth, containerHeight;
+var geometry, geometry2, material, material2, mesh, light;
 
 window.onload = function(){
   init();
@@ -11,8 +11,19 @@ window.onload = function(){
 function init() {
   scene = new THREE.Scene();
 
-  camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
-  camera.position.z = 1000;
+  container = document.getElementsByClassName("container")[0];
+  var containerStyles = window.getComputedStyle(container);
+  containerWidth = parseInt(containerStyles.getPropertyValue("width"));
+  containerHeight = parseInt(containerStyles.getPropertyValue("height"));
+
+  camera1 = new THREE.PerspectiveCamera(75, (containerWidth / 2) / (containerHeight / 2), 1, 10000);
+  camera1.position.z = 500;
+  camera1.up.set(0, 1, 0);
+
+  
+  camera2 = new THREE.PerspectiveCamera(75, (containerWidth / 2) / (containerHeight / 2), 1, 10000);
+  camera2.position.x = 800;
+  camera2.up.set(0, 1, 0);
 
   geometry = new THREE.BoxGeometry(200, 200, 200);
   material = new THREE.MeshBasicMaterial({
@@ -20,17 +31,51 @@ function init() {
     wireframe: true
   });
 
-  mesh = new THREE.Mesh(geometry, material);
-  scene.add(mesh);
+  geometry2 = new THREE.BoxGeometry(300, 300, 300);
+  material2 = new THREE.MeshBasicMaterial({
+    color: 0xAFC916,
+    wireframe: true
+  });
+
+
+
+  cube1 = new THREE.Mesh(geometry, material);
+  cube1.position.set(0, 100, 0);
+  scene.add(cube1);
+
+  cube2 = new THREE.Mesh(geometry2, material2);
+  cube2.position.set(400, 100, 0);
+  scene.add(cube2);
+
+  light = new THREE.DirectionalLight(0xFFFFFF, 1);
+  light.position.set(0, 350, 0);
+  scene.add(light);
+
+
 
   renderer = new THREE.WebGLRenderer();
-  renderer.setSize( window.innerWidth, window.innerHeight);
+  renderer.setSize( containerWidth, containerHeight);
 
-  document.body.appendChild(renderer.domElement);
+  container.appendChild(renderer.domElement);
 }
 
 function animate() {
+  render();
   requestAnimationFrame( animate);
 
-  renderer.render(scene, camera);
+}
+
+function render() {
+  renderer.setViewport(0, 0, (containerWidth / 2), containerHeight);
+  renderer.setScissor(0, 0, (containerWidth / 2), containerHeight);
+  renderer.enableScissorTest(true);
+  camera1.aspect = (containerWidth / 2) /containerHeight;
+  renderer.render(scene, camera1);
+
+  renderer.setViewport((0.5 * containerWidth), 0, (containerWidth / 2), containerHeight);
+  renderer.setScissor((0.5 * containerWidth), 0, (containerWidth / 2), containerHeight);
+  renderer.enableScissorTest(true);
+  camera1.aspect = (containerWidth / 2) /containerHeight;
+  renderer.render(scene, camera2);
+
 }

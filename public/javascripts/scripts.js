249bdc04434057a886c2a637dcc834c33556df24
mike590 <mike.gherard@gmail.com>
2015-02-05 14:42:06 -0500

    Removed the walls of the gamebox and renamed it floor. Repoisitioned the camera above the floor and set it to look at the floor.

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,7 +1,7 @@
 console.log("Loaded!")
 
 var scene, camera, renderer, container, containerWidth, containerHeight;
-var geometry, material, gameBox, light;
+var geometry, material, floor, light;
 
 window.onload = function(){
   init();
@@ -17,32 +17,19 @@ function init() {
   containerHeight = parseInt(containerStyles.getPropertyValue("height"));
 
 
+  // Making geometry of floor
   geometry = new THREE.Geometry();
-  geometry.colorsNeedUpdate = true;
-  // Making geometry of gamebox, which will be a column with a floor and no ceiling
-  // 8 Vertices of Rectangular Prism
+  // 4 Vertices of Rectangle
   geometry.vertices.push(new THREE.Vector3(0, 0, 0));
   geometry.vertices.push(new THREE.Vector3(300, 0, 0));
   geometry.vertices.push(new THREE.Vector3(0, 0, 300));
   geometry.vertices.push(new THREE.Vector3(300, 0, 300));
-  geometry.vertices.push(new THREE.Vector3(0, 1000, 0));
-  geometry.vertices.push(new THREE.Vector3(300, 1000, 0));
-  geometry.vertices.push(new THREE.Vector3(0, 1000, 300));
-  geometry.vertices.push(new THREE.Vector3(300, 1000, 300));
+  
   
   // Floor
   geometry.faces.push(new THREE.Face3(0, 1, 2));
   geometry.faces.push(new THREE.Face3(3, 2, 1));
-  // Four Sides
-  geometry.faces.push(new THREE.Face3(0, 1, 4));
-  geometry.faces.push(new THREE.Face3(1, 4, 5));
-  geometry.faces.push(new THREE.Face3(0, 2, 4));
-  geometry.faces.push(new THREE.Face3(2, 4, 6));
-  geometry.faces.push(new THREE.Face3(2, 3, 6));
-  geometry.faces.push(new THREE.Face3(3, 6, 7));
-  geometry.faces.push(new THREE.Face3(3, 7, 1));
-  geometry.faces.push(new THREE.Face3(1, 5, 7));
-  geometry.faces[4].color.setHex('#F7AF14');
+ 
 
 
 
@@ -54,13 +41,13 @@ function init() {
 
 
 
-  gameBox = new THREE.Mesh(geometry, material);
-  gameBox.position.set(0, 0, 0);
-  scene.add(gameBox);
+  floor = new THREE.Mesh(geometry, material);
+  floor.position.set(0, 0, 0);
+  scene.add(floor);
 
   camera = new THREE.PerspectiveCamera(75, (containerWidth / containerHeight), 1, 1000);
-  camera.position.set(600, 500, 800);
-  camera.lookAt(new THREE.Vector3(0, 500, 0));
+  camera.position.set(0, 500, 0);
+  camera.lookAt(new THREE.Vector3(0, 0, 0));
 
 
 
